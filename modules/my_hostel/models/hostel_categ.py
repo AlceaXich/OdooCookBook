fd617d33b6785210e5f5d61ccd57e8f34e6127f3
XichShim <shirmoc.t05@gmail.com>
2024-11-16 11:41:42 -0500

    Creación de nuevos registros desde otro modelo.

--- modules/my_hostel/models/hostel_categ.py
+++ modules/my_hostel/models/hostel_categ.py
@@ -3,23 +3,52 @@ from odoo.exceptions import ValidationError
 
 
 class HostelCategory(models.Model):
-    _name = "hostel.category"
-    _description = "Hostel Categories"
-    _parent_store = True
-    _parent_name = "parent_id" # optional if field is 'parent_id'
+	_name = "hostel.category"
+	_description = "Hostel Categories"
+	_parent_store = True
+	_parent_name = "parent_id" # optional if field is 'parent_id'
 
-    name = fields.Char('Categoría')
-    parent_id = fields.Many2one(
-        'hostel.category',
-        string='Categoría padre',
-        ondelete='restrict',
-        index=True)
-    parent_path = fields.Char(index=True)
-    child_ids = fields.One2many(
-        'hostel.category', 'parent_id',
-        string='Categorías hijas')
+	name = fields.Char('Categoría')
+	parent_id = fields.Many2one(
+		'hostel.category',
+		string='Categoría padre',
+		ondelete='restrict',
+		index=True)
+	parent_path = fields.Char(index=True)
+	child_ids = fields.One2many(
+		'hostel.category', 'parent_id',
+		string='Categorías hijas')
 
-    @api.constrains('parent_id')
-    def _check_hierarchy(self):
-        if not self._check_recursion():
-            raise models.ValidationError('Error! You cannot create recursive categories.')
+	@api.constrains('parent_id')
+	def _check_hierarchy(self):
+		if not self._check_recursion():
+			raise models.ValidationError('Error! You cannot create recursive categories.')
+
+	def create_categories(self):
+		#Creamos un diccionario para la primera categoria hija
+		categ1 = {
+			'name': 'Categoria hija 1',
+			'description': 'Descripción de la categoria hija 1'
+		}
+		#Creamos un diccionario para la segunda categoria hija
+		categ2 = {
+			'name': 'Child category 2',
+			'description': 'Description for child 2'
+		}
+		#Creamos un diccionario para la categoria principal
+		parent_category_val = {
+			'name': 'Categoría padre',
+			'description': 'Descripción de la categoría padre',
+			'child_ids': [
+				(0, 0, categ1),
+				(0, 0, categ2),
+			]
+		}
+		#Llame al método create() para crear los nuevos registros
+		record = self.env['hostel.room.category'].create(parent_category_val)
+
+		#Tambien admite la creación de registros en un lote.
+		#multiple_records = self.env['hostel.room.category'].create([categ1, categ2])
+
+
+
--- modules/my_hostel/models/hostel_room.py
+++ modules/my_hostel/models/hostel_room.py
@@ -9,38 +9,58 @@ class BaseArchive(models.AbstractModel):
 			record.active = not record.active
 
 class HostelRoom(models.Model):
-    _name = "hostel.room"
-    _description = "Imformacióndelahabitación"
-    _rec_name = "room_no"
+	_name = "hostel.room"
+	_description = "Imformacióndelahabitación"
+	_rec_name = "room_no"
 
-    @api.depends("student_per_room", "student_ids")
-    def _compute_check_availability(self):
-        """Method to check room availability"""
-        for rec in self:
-            rec.availability = rec.student_per_room - len(rec.student_ids.ids)
+	@api.depends("student_per_room", "student_ids")
+	def _compute_check_availability(self):
+		"""Method to check room availability"""
+		for rec in self:
+			rec.availability = rec.student_per_room - len(rec.student_ids.ids)
 
-    name = fields.Char(string="Nombre", required=True)
-    room_no = fields.Char("Nro. habitación", required=True)
-    floor_no = fields.Integer("Nro. piso", default=1, help="Floor Number")
-    currency_id = fields.Many2one('res.currency', string='Moneda')
-    rent_amount = fields.Monetary('Alquiler', help="Enter rent amount per month") # optional attribute: currency_field='currency_id' incase currency field have another name then 'currency_id'
-    hostel_id = fields.Many2one("hostel.hostel", "hostel", help="Name of hostel")
-    student_ids = fields.One2many("hostel.student", "room_id",
-        string="Estudiantes", help="Enter students")
-    hostel_amenities_ids = fields.Many2many("hostel.amenities",
-        "hostel_room_amenities_rel", "room_id", "amenitiy_id",
-        string="Amenities", domain="[('active', '=', True)]",
-        help="Select hostel room amenities")
-    student_per_room = fields.Integer("Estudiantes por habitación", required=True,
-        help="Students allocated per room")
-    availability = fields.Float(compute="_compute_check_availability",
-        store=True, string="Availability", help="Room availability in hostel")
+	name = fields.Char(string="Nombre", required=True)
+	room_no = fields.Char("Nro. habitación", required=True)
+	floor_no = fields.Integer("Nro. piso", default=1, help="Floor Number")
+	currency_id = fields.Many2one('res.currency', string='Moneda')
+	rent_amount = fields.Monetary('Alquiler', help="Enter rent amount per month") # optional attribute: currency_field='currency_id' incase currency field have another name then 'currency_id'
+	hostel_id = fields.Many2one("hostel.hostel", "hostel", help="Name of hostel")
+	student_ids = fields.One2many("hostel.student", "room_id",
+		string="Estudiantes", help="Enter students")
+	hostel_amenities_ids = fields.Many2many("hostel.amenities",
+		"hostel_room_amenities_rel", "room_id", "amenitiy_id",
+		string="Amenities", domain="[('active', '=', True)]",
+		help="Select hostel room amenities")
+	student_per_room = fields.Integer("Estudiantes por habitación", required=True,
+		help="Students allocated per room")
+	availability = fields.Float(compute="_compute_check_availability",
+		store=True, string="Availability", help="Room availability in hostel")
 
-    _sql_constraints = [
-       ("room_no_unique", "unique(room_no)", "Room number must be unique!")]
+	_sql_constraints = [
+	   ("room_no_unique", "unique(room_no)", "Room number must be unique!")]
 
-    @api.constrains("rent_amount")
-    def _check_rent_amount(self):
-        """Constraint on negative rent amount"""
-        if self.rent_amount < 0:
-            raise ValidationError(_("Rent Amount Per Month should not be a negative value!"))
+	@api.constrains("rent_amount")
+	def _check_rent_amount(self):
+		"""Constraint on negative rent amount"""
+		if self.rent_amount < 0:
+			raise ValidationError(_("Rent Amount Per Month should not be a negative value!"))
+
+	def log_all_room_members(self):
+		#Este metodo obtiene todos los miembros de la habitación
+		#del modelo hostel.room.member
+		hostel_room_obj = self.env['hostel.room.member']
+		all_members = hostel_room_obj.search([])
+		print("Obtenemos los registros del modelo hostel.room.member", all_members)
+		return True
+
+
+class HostelRoomMember(models.Model):
+	_name = 'hostel.room.member'
+	_inherits = {'res.partner': 'partner_id'}
+	_description = "Miembros de hostel Room"
+
+	partner_id = fields.Many2one('res.partner', ondelete='cascade')
+	date_start = fields.Date('Fecha inicio')
+	date_end = fields.Date('Fecha fin')
+	member_number = fields.Char()
+	date_of_birth = fields.Date('Fecha de cumpleaños')
